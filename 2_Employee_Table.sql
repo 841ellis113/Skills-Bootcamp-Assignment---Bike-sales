2829523a8805c9174d392de0ae84ac37a20a2901
Michael Ellis <m.ellis841113@gmail.com>
2025-11-27 16:56:46 +0000

    Delete 2_Employee_Table.sql

--- 2_Employee_Table.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE [dbo].[tblEmployees](
-	[EmployeeID] [int] IDENTITY(1,1) NOT NULL PRIMARY KEY,
-	[FirstName] [nvarchar](40) NOT NULL,
-	[MiddleInitial] [nvarchar](40) NULL,
-	[LastName] [nvarchar](40) NOT NULL,
-);
